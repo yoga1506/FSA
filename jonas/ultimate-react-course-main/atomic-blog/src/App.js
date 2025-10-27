820cb23f3b9073b2cc6dfb245295a50e06b1b880
yoga1506 <gvyogaraj@gmail.com>
2025-10-27 16:53:12 +0530

    added

--- jonas/ultimate-react-course-main/atomic-blog/src/App.js
+++ jonas/ultimate-react-course-main/atomic-blog/src/App.js
@@ -10,8 +10,9 @@ function createRandomPost() {
 
 function App() {
   const [posts, setPosts] = useState(() =>
-    Array.from({ length: 30 }, () => createRandomPost())
+    Array.from({ length: 10 }, () => createRandomPost())
   );
+
   const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
 
@@ -58,7 +59,7 @@ function App() {
         setSearchQuery={setSearchQuery}
       />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive onAddPost={handleAddPost} />
+      {/* <Archive onAddPost={handleAddPost} /> */}
       <Footer />
     </section>
   );
@@ -155,37 +156,36 @@ function List({ posts }) {
   );
 }
 
-function Archive({ onAddPost }) {
-  // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
-  const [posts] = useState(() =>
-    // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
-    Array.from({ length: 10000 }, () => createRandomPost())
-  );
+// function Archive({ onAddPost }) {
+//   const [posts] = useState(() =>
+//     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
+//     Array.from({ length: 10000 }, () => createRandomPost())
+//   );
 
-  const [showArchive, setShowArchive] = useState(false);
+//   const [showArchive, setShowArchive] = useState(false);
 
-  return (
-    <aside>
-      <h2>Post archive</h2>
-      <button onClick={() => setShowArchive((s) => !s)}>
-        {showArchive ? "Hide archive posts" : "Show archive posts"}
-      </button>
+//   return (
+//     <aside>
+//       <h2>Post archive</h2>
+//       <button onClick={() => setShowArchive((s) => !s)}>
+//         {showArchive ? "Hide archive posts" : "Show archive posts"}
+//       </button>
 
-      {showArchive && (
-        <ul>
-          {posts.map((post, i) => (
-            <li key={i}>
-              <p>
-                <strong>{post.title}:</strong> {post.body}
-              </p>
-              <button onClick={() => onAddPost(post)}>Add as new post</button>
-            </li>
-          ))}
-        </ul>
-      )}
-    </aside>
-  );
-}
+//       {showArchive && (
+//         <ul>
+//           {posts.map((post, i) => (
+//             <li key={i}>
+//               <p>
+//                 <strong>{post.title}:</strong> {post.body}
+//               </p>
+//               <button onClick={() => onAddPost(post)}>Add as new post</button>
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//     </aside>
+//   );
+// }
 
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
--- projects/react-tailwind-templete/src/App.jsx
+++ projects/react-tailwind-templete/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <>hello</>;
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
 };
 
 export default App;
