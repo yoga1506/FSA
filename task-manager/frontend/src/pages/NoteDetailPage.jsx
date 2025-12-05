5b4d47d947cd05cdda11ab16e41c9824342a4eb9
yoga1506 <gvyogaraj@gmail.com>
2025-12-05 17:10:23 +0530

    crud operation done

--- task-manager/frontend/src/pages/NoteDetailPage.jsx
+++ task-manager/frontend/src/pages/NoteDetailPage.jsx
@@ -1,23 +1,22 @@
-import React, { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Link, useNavigate, useParams } from "react-router";
-
-import toast, { LoaderIcon } from "react-hot-toast";
 import api from "../lib/axios";
-import { ArrowLeft, Trash2Icon } from "lucide-react";
+import toast from "react-hot-toast";
+import { ArrowLeftIcon, LoaderIcon, Trash2Icon } from "lucide-react";
 
 const NoteDetailPage = () => {
   const [note, setNote] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [saving, setSaving] = useState(false);
 
   const navigate = useNavigate();
 
   const { id } = useParams();
-  console.log(id);
+
   useEffect(() => {
     const fetchNote = async () => {
       try {
-        setLoading(true);
         const res = await api.get(`/notes/${id}`);
         setNote(res.data);
       } catch (error) {
@@ -30,7 +29,36 @@ const NoteDetailPage = () => {
 
     fetchNote();
   }, [id]);
-  console.log(note);
+
+  const handleDelete = async () => {
+    if (!window.confirm("Are you sure you want to delete this nots?")) return;
+    try {
+      await api.delete(`/notes/${id}`);
+      toast.success("Notes deleted Sucessfully");
+      navigate("/");
+    } catch (error) {
+      console.log("Error a delete api", error);
+
+      toast.error("Error occured on deleting the note");
+    }
+  };
+  const handleSave = async () => {
+    if (!note.title.trim() || !note.title.trim()) {
+      toast.error("Please add a title or content");
+      return;
+    }
+    setSaving(true);
+    try {
+      await api.put(`notes/${id}`, note);
+      toast.success("Notes Uppated Succesfully");
+      navigate("/");
+    } catch (error) {
+      console.log("Error  saving notes", error);
+      toast.error("Failed To update notes");
+    } finally {
+      setSaving(false);
+    }
+  };
 
   if (loading) {
     return (
@@ -39,54 +67,59 @@ const NoteDetailPage = () => {
       </div>
     );
   }
-  function handleDelete() {}
-  function handleSave() {}
 
   return (
     <div className="min-h-screen bg-base-200">
       <div className="container mx-auto px-4 py-8">
         <div className="max-w-2xl mx-auto">
-          <div className="flex justify-between items-center mb-6">
-            <Link to={"/"} className="btn btn-ghost">
-              <ArrowLeft className="h-5 w-5" /> back to notes
+          <div className="flex items-center justify-between mb-6">
+            <Link to="/" className="btn btn-ghost">
+              <ArrowLeftIcon className="h-5 w-5" />
+              Back to Notes
             </Link>
             <button
               onClick={handleDelete}
-              className="btn btn-outline btn-error"
+              className="btn btn-error btn-outline"
             >
-              <Trash2Icon className="h-5 w-5 " />
+              <Trash2Icon className="h-5 w-5" />
               Delete Note
             </button>
           </div>
-          <div className=" card body bg-base-100">
+
+          <div className="card bg-base-100">
             <div className="card-body">
               <div className="form-control mb-4">
                 <label className="label">
                   <span className="label-text">Title</span>
                 </label>
                 <input
-                  className="input input-bordered"
-                  placeholder="Note Title"
                   type="text"
+                  placeholder="Note title"
+                  className="input input-bordered"
+                  value={note.title}
                   onChange={(e) => setNote({ ...note, title: e.target.value })}
                 />
               </div>
+
               <div className="form-control mb-4">
                 <label className="label">
                   <span className="label-text">Content</span>
                 </label>
                 <textarea
-                  placeholder="write your notes here"
+                  placeholder="Write your note here..."
                   className="textarea textarea-bordered h-32"
                   value={note.content}
-                  onChange={(e) => setNote({ ...note, title: e.target.value })}
+                  onChange={(e) =>
+                    setNote({ ...note, content: e.target.value })
+                  }
                 />
               </div>
-              <div className="flex justify-end">
+
+              <div className="card-actions justify-end">
                 <button
-                  onClick={handleSave}
-                  disabled={saving}
                   className="btn btn-primary"
+                  disabled={saving}
+                  onClick={handleSave}
                 >
                   {saving ? "Saving..." : "Save Changes"}
                 </button>
@@ -98,5 +131,4 @@ const NoteDetailPage = () => {
     </div>
   );
 };
-
 export default NoteDetailPage;
