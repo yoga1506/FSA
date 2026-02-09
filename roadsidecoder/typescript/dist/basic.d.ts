b35169a24e3c9c7321f6de7a55accadfe59c93eb
yoga1506 <gvyogaraj@gmail.com>
2026-02-09 17:45:49 +0530

    added

---
+++ roadsidecoder/typescript/dist/basic.d.ts
@@ -0,0 +1,2 @@
+declare const role: string;
+//# sourceMappingURL=basic.d.ts.map
---
+++ roadsidecoder/typescript/src/basic.ts
@@ -0,0 +1,17 @@
+const role: string = "fullstack";
+console.log(role);
+
+// primitive
+let userName: string = "yogaraj";
+let age: number = 33;
+
+let isAdmin: boolean = true;
+
+// array of number
+let numbers: number[] = [1, 2, 3, 4, 5];
+let fruits: string[] = ["apple", "grape", "banana"];
+
+// tuples
+let person: [string, number] = ["yoga", 33];
+
+// enum
