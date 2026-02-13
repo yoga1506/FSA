0378eebd5dd01b0e90d1eed1987817d0a68b4f03
yoga1506 <gvyogaraj@gmail.com>
2026-02-13 16:50:28 +0530

    working on redux

--- jonas-schemmedman/ultimate-react-course-main/fast-pizza-co/src/ui/Header.jsx
+++ jonas-schemmedman/ultimate-react-course-main/fast-pizza-co/src/ui/Header.jsx
@@ -5,7 +5,7 @@ import UserName from "../features/user/userName";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-10 sm:px-6">
       <Link className="tracking-widest" to="/">
         The Pizza Co.
       </Link>
--- jonas-schemmedman/ultimate-react-course-main/fast-pizza-co/src/ui/Home.jsx
+++ jonas-schemmedman/ultimate-react-course-main/fast-pizza-co/src/ui/Home.jsx
@@ -1,18 +1,17 @@
 import CreateUser from "../features/user/CreateUser";
-import Button from "./Button";
 
 function Home() {
   return (
     <div className="my-10 px-4 py-16 text-center sm:my-16">
-      <h1 className="mb-4 text-center text-xl font-semibold text-stone-700 md:text-3xl">
+      <h1 className="mb-6 text-center text-xl font-semibold text-stone-700 md:text-3xl">
         The best pizza.
         <br />
-        <span className="text-yellow-500">
+        <span className=" text-yellow-500">
           Straight out of the oven, straight to you.
         </span>
       </h1>
 
-      {/* <CreateUser /> */}
+      <CreateUser />
     </div>
   );
 }
