d92f8a8aed2eab0575ca29d316a14d5b415a9f86
yoga1506 <gvyogaraj@gmail.com>
2026-03-20 17:24:14 +0530

    added

---
+++ codesistency/100projects/01.quiz game/script.js
@@ -0,0 +1,6 @@
+const startBtn = document.getElementsByClassName("start-quiz");
+const questionScreen = document.getElementById("question-screen");
+
+startBtn.addEventListner("click", function () {
+  alert("hello");
+});
