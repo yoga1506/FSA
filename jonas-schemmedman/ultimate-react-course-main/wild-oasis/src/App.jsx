11dcddd3464cc61867a2b7c5f561454465026eb2
Your Name <yogarajvedagiri@gmail.com>
2026-04-13 09:44:02 +0530

    added

--- jonas-schemmedman/ultimate-react-course-main/wild-oasis/src/App.jsx
+++ jonas-schemmedman/ultimate-react-course-main/wild-oasis/src/App.jsx
@@ -1,22 +1,48 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const H1 = styled.h1`
-  font-size: 40px;
+  font-size: 30px;
   font-weight: 600;
 `;
 
 const Button = styled.button`
-  font-size: 20px;
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
   background-color: purple;
-  color: white;
+  color: var(--color-brand-50);
+  box-shadow: var(--shadow-sm);
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid var(--color-gray-300);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: var(--color-brand-500);
+  padding: 10px 20px;
 `;
 
 const App = () => {
   return (
-    <div>
-      <H1>Wild oasis</H1>
-      <Button>Submit</Button>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Wild oasis</H1>
+        <Button>Submit</Button>
+        <Input placeholder="enter text" />
+      </StyledApp>
+    </>
   );
 };
 
