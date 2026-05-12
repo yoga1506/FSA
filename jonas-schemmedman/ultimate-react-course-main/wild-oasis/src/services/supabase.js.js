68c855943fec68544962c9ee4c2a0eab6631fcaa
Your Name <yogarajvedagiri@gmail.com>
2026-05-12 21:35:09 +0530

    added

--- jonas-schemmedman/ultimate-react-course-main/wild-oasis/src/services/supabase.js.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://ksqpupzmiscbqlesrtfw.supabase.co";
-const supabaseKey = "sb_publishable_OaByoKzUNO4Vf8NmqC2DgQ_Q8X1KnKJ";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
