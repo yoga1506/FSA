51b542d4dedf4ad4e98f614afc650a1afdbd54a5
Your Name <yogarajvedagiri@gmail.com>
2026-05-14 08:49:47 +0530

    added

---
+++ jonas-schemmedman/wild-oasis/src/App.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 32px;
+`;
+
+const App = () => {
+  return (
+    <div>
+      <H1>app</H1>
+    </div>
+  );
+};
+
+export default App;
